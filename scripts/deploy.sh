d39e866760ed549a04e4c302789db55264e389ef
mateuszlazar <mateuszlazar@outlook.com>
2021-03-13 18:39:18 +0100

    feat: introduce gatsby

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,10 +8,10 @@ DEPLOY_BRANCH='gh-pages'
 DIR=$(dirname "$(readlink -f "$0")")
 
 git fetch -f . $BUILD_BRANCH:$DEPLOY_BRANCH
-git checkout $DEPLOY_BRANCH || git checkout $BUILD_BRANCH
-git add -f dist || git checkout $BUILD_BRANCH
-git commit -m "deploy to $DEPLOY_BRANCH" || git checkout $BUILD_BRANCH
-git push origin `git subtree split --prefix dist`:$DEPLOY_BRANCH --force
+git checkout $DEPLOY_BRANCH || git checkout -b $BUILD_BRANCH
+git add -f public
+git commit -m "deploy to $DEPLOY_BRANCH"
+git push origin `git subtree split --prefix public`:$DEPLOY_BRANCH --force
 git checkout $BUILD_BRANCH
 
 echo -e '\n \U0002705 Deployment has been finished. \n'
